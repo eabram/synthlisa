862179f141dd9b233b90edfe1439d1deddc8e28d
Ester Abram <esterabram@hotmail.com>
2018-11-09 12:32:13 +0100

    Complete PAA package

--- calculations/PAA_LISA/PAA_LISA/__init__.py
+++ calculations/PAA_LISA/PAA_LISA/__init__.py
@@ -1,16 +1,8 @@
-from synthlisa import *
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-from fractions import Fraction
-import math
-import datetime
-from scipy.interpolate import interp1d
-from scipy.interpolate import RegularGridInterpolator
+from .imports import *
 
 from .class_orbit import orbit
-from .PAA import PAA
-from .PAA import la
+from .calc import PAA
+from .calc import la
 
 import warnings
 warnings.filterwarnings('ignore', 'The iteration is not making good progress')
